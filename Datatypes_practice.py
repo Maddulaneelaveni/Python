b3e437291c616dd624c73aa78bb967cd0fb94e0a
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-02 23:13:26 +0530

    new file added

---
+++ Datatypes_practice.py
@@ -0,0 +1,41 @@
+# 1. Check the Type of a variable
+a=10   #int
+b=5.5  # float
+c="Neelaveni"  # string
+d=True        # bool
+list=[1,2,3,4]
+set={10,20,30}
+tuple=(8,9,10)
+dictionaries={"name": "Neelu", "city": "Hyderabad"}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(list))
+print(type(set))
+print(type(tuple))
+print(type(dictionaries))
+
+# 2. Integer and float sum
+x=5
+y=10.5
+z=x+y
+print(z)
+print(type(z))
+
+# 3.Covert String to integer
+s="25"
+num=int(s)
+result=num+5
+print(result)
+
+# 4. Convert Integer to String
+n=100
+s=str(n) + "apples"
+print(s)
+
+# 5. Boolean example
+check=10>5
+print("value:", check)
+print(type(check))
+
