58eb69f06c408b934a6fc4b259ef86ebff7a8843
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-02 16:42:00 +0530

    new file added

---
+++ Variables_problems.py
@@ -0,0 +1,85 @@
+# 1. Swap two variables
+a=3
+b=10
+a,b=b,a
+print("a:", a, "b:", b)
+
+# 2. Area of Rectangle
+length = 5
+breadth = 10
+area = length * breadth
+print("Area of rectangel:", area)
+
+# 3. Perimeter of a Square
+side = 5
+perimeter = 4 * side
+print("Perimeter of a square:", perimeter)
+
+# 4. Average of 3 numbers
+a=5
+b=10
+c=15
+average = (a+b+c) / 3
+print("Average of three numbers:", average)
+
+# 5. Area of Circle
+radius=4
+area=3.14 * radius * radius
+print("Area of circle:", area)
+
+# 6. Convert hours to minutes
+hours=4
+minutes=hours * 60
+print("Minutes:", minutes)
+
+# 7. Sum of 2 numbers
+a=20
+b=40
+sum=a + b
+print("Sum:", sum)
+
+# 8.Simple Interest
+principal=1500
+rate=5
+time=2
+SI = (principal*rate*time)/100
+print("Simple Interest:", SI)
+
+#9.Temparature Conversion 
+C=30
+F=(c * 9/5) +32
+print("Fahrenheit:", F)
+
+# 10. Calculate Remainder
+num1=10
+num2=5
+remainder=num1%num2
+print("Remainder:", remainder)
+
+# 11.Convert Kilometers to Meters
+km=5
+meters=km * 1000
+print("Meters", meters)
+
+# 12.Calculate Speed
+distance=100
+time=2
+speed=distance/time
+print("Speed:", speed, "km/h")
+
+# 13.Convert Rupees into Paise
+rupees=5
+paise=rupees * 100
+print("Paise:", paise)
+
+# 14.Calculate BMI
+weight=50
+height=1.65 #meters
+BMI=weight/(height*height)
+print("BMI:", BMI)
+
+# 15.Calculate Final price with discount
+price=1000
+discount=10
+final_price=price-(price*discount/100)
+print("Final Price:", final_price)
