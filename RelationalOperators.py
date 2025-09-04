bae96c536f7e098642050ba089ebac0a6266b4b5
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-04 22:48:09 +0530

    new file added

---
+++ RelationalOperators.py
@@ -0,0 +1,33 @@
+# Relational Operators
+# Used to compare two values. Returns True or False.
+# Include ==, !=, >, <, >=, <=
+# Used in grading, pass/fail checks, eligibility tests.
+
+# 1.Check if a student’s marks are greater than or equal to passing marks.
+# Student pass/fail
+marks = 75
+passing_marks = 40
+print("Passed?", marks >= passing_marks)  # True
+
+
+# 2.Overspeeding check
+
+speed = 85
+print("Overspeeding?", speed > 80)  # True
+
+# 3.Petrol price comparison
+# Check if petrol price today is greater than yesterday.
+today_price = 110
+yesterday_price = 108
+print("Increased?", today_price > yesterday_price)  # True
+
+# 4.Product rating
+# A product’s rating is 4.5 – check if it is greater than 4.0 (good rating).
+rating = 4.5
+print("Good rating?", rating > 4.0)  # True
+
+# 5.Boxing qualification (weight check)
+
+weight = 48
+print("Qualified?", weight <= 50)  # True
+
