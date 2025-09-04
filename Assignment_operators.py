972f0da56211459a3bd0dc0386785f0adc8d2500
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-04 22:31:01 +0530

    new file added

---
+++ Assignment_operators.py
@@ -0,0 +1,66 @@
+# Arithmetic operators are used for mathematical calculations.
+# Arithmetic operators include +, -, *, /, //, %, **
+# used in billing systems (shopping bill), finance and shopping apps.
+
+# Problems
+
+# 1.Calculate the area of a rectangle given length and width
+l=5
+b=3
+area=l*b
+print("Area of a rectangle:", area)
+
+# 2. Calculating Bill
+# 2.A shopkeeper sells an item for ₹250 each. Find the bill for 12 items.
+price = 250
+quantity = 12
+bill = price * quantity
+print("Total bill:", bill)
+
+
+# 3. Monthly salary after tax
+# A person’s monthly salary is ₹40,000. After a 10% tax deduction, calculate take-home pay.
+
+salary = 40000
+tax = salary * 10/100
+take_home = salary - tax
+print("Take Home Salary:", take_home)  
+
+
+# 4. Calculating Average speed
+# A car travels 540 km in 9 hours. Find average speed.
+
+distance = 540
+time = 9
+avg_speed = distance / time
+print("Average Speed:", avg_speed)  
+
+# 5.Average cricket score
+# A cricket player scored runs in 5 matches. Find his average score.
+
+scores = [45, 60, 35, 80, 50]
+average = sum(scores) / len(scores)
+print("Average Score:", average)  
+
+
+# 6.Student percentage
+# A student has marks in 6 subjects. Find the total and percentage.
+
+marks = [80, 75, 90, 85, 70, 95]
+total = sum(marks)
+percentage = (total / (6 * 100)) * 100
+print("Percentage:", percentage)  
+
+
+# 7.Employee profit share
+# A company earns ₹5,00,000 profit and distributes it equally among 25 employees. Find per employee share.
+
+profit = 500000
+employees = 25
+share = profit / employees
+print("Share per employee:", share)  
+
+
+
+
+
