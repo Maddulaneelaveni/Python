cc81571ab1dcb63d72761d91f53deb483ce53dce
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-06 21:14:05 +0530

    new file added

---
+++ MembershipOperators.py
@@ -0,0 +1,25 @@
+# 1.Check if fruit is in basket (in)
+
+basket = ["apple", "banana", "mango"]
+print("apple" in basket)  # True
+
+
+# 2.Student enrollment check (not in)
+students = ["Ravi", "Neela", "Ankit"]
+print("Rahul" not in students)  # True
+
+
+# 3.Word search in sentence (in)
+sentence = "Python is fun"
+print("fun" in sentence)  # True
+
+
+# 4.Library book search (in)
+library = ["Harry Potter", "Python Basics", "AI Revolution"]
+print("AI Revolution" in library)  # True
+
+
+# 5. Digit check in mobile number (in)
+mobile = "9876543210"
+print("5" in mobile)  # True
+
