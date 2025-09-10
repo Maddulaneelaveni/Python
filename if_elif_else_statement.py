dbbbec9550d510a9ea27e9cc8651646d7c033a2d
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-10 11:00:00 +0530

    code update

--- if_elif_else_statement.py
+++ if_elif_else_statement.py
@@ -32,23 +32,6 @@ else:
 
 
 
-# 4. simple calculator
-
-a=float(input("Enter first number: "))
-b=float(input("Enter second number: "))
-op=input("Enter operator (+, -, *, /): ")
-if op=='+':
-    print("Result:", a + b)
-elif op=='-':
-    print("Result:", a - b)
-elif op=='*':
-    print("Result:", a * b)
-elif op=='/':
-    print("Result:", a / b)
-else:
-    print("Invalid operator")
-
-
 
 
 
