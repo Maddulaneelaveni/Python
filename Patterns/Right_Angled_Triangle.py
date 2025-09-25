c86aa24cb87054fbac3f5b39c6d746ddc4b869d1
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-09-25 22:23:37 +0530

    new file added

---
+++ Patterns/Right_Angled_Triangle.py
@@ -0,0 +1,30 @@
+# Right angled triangle pattern
+
+n = 5  # no.of rows
+for i in range(1, n+1):  # loop from 1 to 5
+    print("*" * i)   # print i stars in each row
+
+# output:
+
+#*
+#**
+#***
+#****
+#*****
+
+
+
+# Inverted right angled triangle
+
+n = 5   # no.of rows
+for i in range(n, 0, -1):  # loop from 5 to 1
+    print("*" * i)  # print i stars in each rrow
+
+# output:
+
+#*****
+#****
+#***
+#**
+#*
+
