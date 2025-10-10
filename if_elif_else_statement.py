ff54b0158371d0b757089ff693788558586586b0
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-10 22:00:00 +0530

    Code update

---
+++ if_elif_else_statement.py
@@ -0,0 +1,57 @@
+# 1.Find the Greatest of Two Numbers
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if a>b:
+    print("First number is greater")
+elif b>a:
+    print("Second number is greater")
+else:
+    print("Both are equal")
+
+
+
+# 2.Check Leap year
+
+year=int(input("Enter a year: "))
+if (year%400==0) or (year%4==0 and year%100!=0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+
+
+
+# 3.Check Vowel or Consonant
+ch=input("Enter a character: ").lower()
+if ch in "aeiou":
+    print("Vowel")
+elif ch.isalpha():
+    print("Consonant")
+else:
+    print("Not an alphabet")
+
+
+
+# 4. simple calculator
+
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+op=input("Enter operator (+, -, *, /): ")
+if op=='+':
+    print("Result:", a + b)
+elif op=='-':
+    print("Result:", a - b)
+elif op=='*':
+    print("Result:", a * b)
+elif op=='/':
+    print("Result:", a / b)
+else:
+    print("Invalid operator")
+
+
+
+
+
+
+
+
