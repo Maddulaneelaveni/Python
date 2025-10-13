4c985c5858b2013dfe396387dc99008964bbba1b
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-13 22:09:14 +0530

    for loop file is added

---
+++ Loops/ForLooop.py
@@ -0,0 +1,39 @@
+# For Loop:
+# A for loop is used to iterate (repeat) over a sequence like:
+
+# a list
+
+# a string
+
+# a tuple
+
+# a range of numbers
+
+# Syntax:
+# for variable in sequence:
+    # code block
+# On each iteration, variable takes one value from the sequence, and the code inside runs.
+
+# Example 1 – Iterating through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# output:
+# apple
+# banana
+# cherry
+
+# Example 2 – Using range()
+for i in range(5):
+    print(i)
+
+# output:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+
+
