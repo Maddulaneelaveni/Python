8df5b39a105eabb26eb9bd6033f6ac74c4a724e3
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-16 22:29:31 +0530

    Floyds Triangle pattern is added

---
+++ patterns2/IncrementingNumberTriangle.py
@@ -0,0 +1,16 @@
+# Continuous Incrementing Number Triangle (Floyd’s Triangle)
+
+n=5   # total number of rows
+num = 1    # starting number
+for i in range(1, n + 1): # Outer loop: for each row (1 to n)
+    for j in range(1, i + 1): # Inner loop : controls how many numbers to print in that row
+        print(num, end=" ")  # print current number on the same line
+        num += 1    # increment number for next print
+    print()   # move to next line after each row
+
+# output:
+#1
+#2 3
+#4 5 6
+#7 8 9 10
+#11 12 13 14 15
