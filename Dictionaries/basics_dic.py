8eb460e8c8c7b6b6123d99a43e73dd9dac4a7933
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-23 22:01:40 +0530

    new file added

---
+++ Dictionaries/basics_dic.py
@@ -0,0 +1,6 @@
+# Dictionary
+# A dictionary in Python is a collection of key–value pairs.
+# It allows you to store data that can be accessed using a unique key instead of an index (like in lists or tuples).
+
+#syntax:
+my_dict = {key1: value1, key2: value2, key3: value3}
