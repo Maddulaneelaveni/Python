0189b8c5270de0891c1e25d5095c3c0896fbe33d
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-24 21:52:38 +0530

    new file added

---
+++ Dictionaries/practice.py
@@ -0,0 +1,10 @@
+# 1.Create and Access Dictionary Elements
+
+student = {"name": "Neelaveni", "age": 22, "course": "Data Science"}
+
+# Accessing elements using keys
+print("Name:", student["name"])     # prints the value of 'name'
+print("Age:", student["age"])       # prints the value of 'age'
+
+
+# 2.
