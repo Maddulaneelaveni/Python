fd0fb1ec89ad4e2d92ac3ecd8a545d6420055bc1
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-24 21:55:08 +0530

    new file added

--- Dictionaries/practice.py
+++ Dictionaries/practice.py
@@ -8,3 +8,8 @@ print("Age:", student["age"])       # prints the value of 'age'
 
 
 # 2.Count Frequency of Each Character in a String
+word = "apple"
+freq = {}  # Empty dictionary to store frequency
+for char in word:
+    freq[char] = freq.get(char, 0) + 1  # get() returns 0 if char not found, then add 1
+print(freq)
