74fbd0c32f0d798de1bd5b989c74eb6f38d33a2c
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-10-30 23:08:00 +0530

    new file added

---
+++ Functions/basics.py
@@ -0,0 +1,9 @@
+# Function
+# A function is a block of code that performs a specific task and can be reused multiple times.
+
+# Syntax:
+def function_name(parameters):
+    # function body
+    return value
+
+
