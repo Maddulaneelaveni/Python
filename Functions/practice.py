7ba0e4a808cd8802f7820ec26622db2d247ce7c6
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-11-04 22:00:00 +0530

    code updated

--- Functions/practice.py
+++ Functions/practice.py
@@ -2,3 +2,13 @@
 def square(num):
     return num * num
 print(square(4))  # Output: 16
+
+
+# 2. Check if a Number is Even or Odd
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(check_even_odd(9))  # Output: Odd
