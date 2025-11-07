f75f4cdcd6fc2071b5f0e726da4e4a9361a9e210
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-11-07 18:27:24 +0530

    new file added

--- Functions/practice.py
+++ Functions/practice.py
@@ -75,3 +75,11 @@ print(max_two(10,25)) # Output: 25
 
 # 8.Check if number is prime
 
+def is_prime(num):
+    if num <=1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+print(is_prime(11))  # Output: True
