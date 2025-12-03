76f182381e3645cad65cef8dabfb50a58f973feb
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-12-03 22:00:00 +0530

    strings file updated

--- Strings/basic.py
+++ Strings/basic.py
@@ -1,2 +1,22 @@
 # What is a string in Python?
 # A string is just text – a sequence of characters.
+name = "Neelaveni"
+city = 'Hyderabad'
+greeting = "Hello, how are you?"
+multiline_string = """This is a string that spans multiple lines using triple quotes."""
+
+# String Operations
+# Concatenation
+full_greeting = greeting + " My name is " + name + " and I live in " + city + "."
+print(full_greeting)
+
+# Repetition
+echo = "Hello! " * 3
+print(echo)
+
+# Accessing Characters
+first_letter = name[0]  # 'N'
+last_letter = name[-1]  # 'i'
+print("First letter:", first_letter)
+print("Last letter:", last_letter)
+
