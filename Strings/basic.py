ecf88c0d1a6458f7a2e799621456fb5bd001db10
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-12-04 22:00:00 +0530

    strings file updateed

--- Strings/basic.py
+++ Strings/basic.py
@@ -26,3 +26,22 @@ substring = name[1:5]  # 'eelav'
 print("Substring from index 1 to 4:", substring)
 
 
+# String Methods
+upper_case = name.upper()
+print("Uppercase:", upper_case)
+
+lower_case = city.lower()
+print("Lowercase:", lower_case)
+
+length = len(greeting)
+print("Length of greeting:", length)
+
+replaced_string = greeting.replace("Hello", "Hi")
+print("Replaced string:", replaced_string)
+
+
+# Finding Substrings
+index_of_hyphen = greeting.find("how")
+print("Index of 'how':", index_of_hyphen)   
+
+
