d017fa07bb8fafaa12c0b8a14b91f3bec8d64a77
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-12-09 22:00:00 +0530

    file updated

---
+++ Strings/Prblms.py
@@ -0,0 +1,19 @@
+# Basic string operations:
+
+# a) Concatenation (joining)
+first = "Gram"
+second = "Ujjwala"
+result = first + " " + second   # "Gram Ujjwala"
+
+
+# b) Repetition
+s = "Ha"
+print(s * 3)   # "HaHaHa"
+
+# c) Accessing characters
+word = "Python"
+first_char = word[0]   # 'P'
+last_char = word[-1]   # 'n'
+print("First character:", first_char)
+print("Last character:", last_char)
+
