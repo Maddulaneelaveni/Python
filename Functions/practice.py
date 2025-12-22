ee77d48832f1786322518eece6b3b771900a2365
Neelaveni <maddulaneelaveni12793@gmail.com>
2025-12-22 22:00:00 +0530

    file updated

--- Functions/practice.py
+++ Functions/practice.py
@@ -133,5 +133,14 @@ def is_armstrong(num):
     sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
     return sum_of_powers == num
 
+# 17. Program showing Local & Global variable difference
+x = 50   # Global variable
+def show():
+    x = 20   # Local variable
+    print("Inside function:", x)
+show()
+print("Outside function:", x)
+# output : Inside funtion:20
+# Outside function:50
 
 
