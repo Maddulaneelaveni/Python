bcca0be8a550d858eb2a32be62e40a6a61825a65
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-01 22:00:00 +0530

    file updated

--- Functions/gfg_Problems.py
+++ Functions/gfg_Problems.py
@@ -90,13 +90,7 @@ print("Nth term of AP is:", nth_term_ap(a, d, n))
 
 #                    (OR)
 
-def nth_term_ap(a, d, n):
-    return a+(n - 1)*d
-# Example
-a = 2 # first term
-d = 3   # common difference
-n = 5  # term number
-print("Nth term of AP is:", nth_term_ap(a, d, n))
+
 
 
 
