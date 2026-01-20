8faaafac10f481084b7bf535ed64e8d2478a6490
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-20 22:00:00 +0530

    file updated

---
+++ Recursion.py
@@ -0,0 +1,4 @@
+# RECURSION:
+# Recursion is a programming technique where a function calls itself to solve a problem step-by-step until a base condition is reached.
+# In simple words:
+#  A function solving a problem by breaking it into smaller versions of the same problem.
