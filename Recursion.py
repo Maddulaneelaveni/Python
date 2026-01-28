91b19ed2fe5c9691db956dbf19e99129ef13953e
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-28 22:00:00 +0530

    recursion file updated

--- Recursion.py
+++ Recursion.py
@@ -100,6 +100,14 @@ def reverse_string(s):
 print(reverse_string("hello"))  # Output: "olleh"
 
 # 7. Check if a string is a palindrome
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+print(is_palindrome("racecar"))  # Output: True
+print(is_palindrome("hello"))    # Output: False
 
 
 
