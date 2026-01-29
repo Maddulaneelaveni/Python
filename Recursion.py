ae91256063e92fb4a44194a4691aa3d372de5f21
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-29 22:00:00 +0530

    recursion file updated

--- Recursion.py
+++ Recursion.py
@@ -85,8 +85,12 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 print(fibonacci(6))  # Output: 8
 
-
-
+# 5. Sum of first N natural numbers
+def sum_n(n):
+    if n == 0:
+        return 0
+    return n + sum_n(n-1)
+print(sum_n(5))  # Output: 15
 
 
 
