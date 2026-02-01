769b1bf2469cbaba0d9200aeb63f6e42afc194b1
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-01 22:00:00 +0530

    recursion file updated

--- Recursion.py
+++ Recursion.py
@@ -79,6 +79,11 @@ def factorial(n):
 print(factorial(5))
 
 # 4. Fibonacci Series
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(6))  # Output: 8
 
 
 
