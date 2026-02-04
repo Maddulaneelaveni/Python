a8fad94c3acd3d8e9d853fe81cd0b0d873153d19
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-04 22:00:00 +0530

    file updated

--- Range.py
+++ Range.py
@@ -1,7 +1,6 @@
 # What is range() in Python?
 # range() is a built-in function used to generate a sequence of integers.
 # It is most commonly used in loops, especially for loops.
-
-👉 Important: range() does not create a list.
-It returns a range object (lazy evaluation → memory efficient).
+# Important: range() does not create a list.
+# It returns a range object (lazy evaluation → memory efficient).
 
