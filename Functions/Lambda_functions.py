86d58a8e7baffa81a66a809dbdb6c8e56c3f68c3
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-07 22:00:00 +0530

    file updated

---
+++ Functions/Lambda_functions.py
@@ -0,0 +1,5 @@
+# Lamda Funstions :
+# A lambda function is a small anonymous function that can take any number of arguments, but can only have one expression.
+
+# Syntax of lambda function:
+# lambda arguments: expression
