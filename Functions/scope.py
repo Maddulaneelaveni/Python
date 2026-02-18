24d5235ebf5cbf0313a78e220f02be1da5906a36
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-18 23:09:02 +0530

    file updated

--- Functions/scope.py
+++ Functions/scope.py
@@ -1,3 +1,9 @@
 # SCOPE : A variable is only available from inside the region it is created. This is called scope.
 # Local Scope
 # A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+# Example of local scope:
+def my_function():
+    x = 10  # x is a local variable
+    print(x)
+my_function()  # Output: 10
+# print(x)  # This will raise an error because x is not defined outside the function
