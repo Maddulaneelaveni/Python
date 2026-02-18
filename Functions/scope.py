8bf4dadefa99c9b1b8151afccbed5ac5c196fe0d
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-18 23:06:51 +0530

    file updated

--- Functions/scope.py
+++ Functions/scope.py
@@ -1,1 +1,3 @@
 # SCOPE : A variable is only available from inside the region it is created. This is called scope.
+# Local Scope
+# A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
