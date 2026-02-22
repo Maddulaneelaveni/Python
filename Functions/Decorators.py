0b3ded31ae6f3f335e8b08fe33e9e24e4c15518e
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-22 22:00:00 +0530

    file updated

---
+++ Functions/Decorators.py
@@ -0,0 +1,5 @@
+# What is a Decorator? 
+# A decorator is a function that modifies the behavior of another function without changing its code.
+# Real-life analogy:
+# Think of a function as a gift .
+# A decorator is the gift wrapper — it adds something extra without touching what’s inside.
