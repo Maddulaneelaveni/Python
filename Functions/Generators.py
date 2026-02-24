a28a034a8497d7946310310bd4a49eefc126a79f
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-24 22:00:00 +0530

    file updated

---
+++ Functions/Generators.py
@@ -0,0 +1,7 @@
+# What is a Generator?
+# A Generator is a special type of function that:
+# Uses yield instead of return
+# Produces values one at a time
+# Remembers its state between calls
+# Is memory efficient
+# 👉 Instead of returning all values at once, it generates values lazily (on demand).
