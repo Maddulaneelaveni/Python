90c173ee7ceda8855d7159327394acfde1af1b0f
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-12 22:00:00 +0530

    new file added

--- Modules/basics.py
+++ Modules/basics.py
@@ -1,2 +1,3 @@
 # What is module ?
 # A module is simply a Python file that contains code which can be reused in other Python programs.
+# Modules can contain functions, classes, and variables that can be imported and used in other Python files.
