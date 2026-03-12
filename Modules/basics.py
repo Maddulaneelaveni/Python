fba4fbb601e4debb2d40959600962efc9284349c
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-12 22:00:00 +0530

    new file added

--- Modules/basics.py
+++ Modules/basics.py
@@ -2,5 +2,8 @@
 # A module is simply a Python file that contains code which can be reused in other Python programs.
 # Modules can contain functions, classes, and variables that can be imported and used in other Python files.
 # Modules are a way to organize and reuse code, making it easier to maintain and share across different projects.
+
 # To use a module, you can import it using the import statement. For example, if you have a module named my_module.py, you can import it in another Python file like this:
 # import my_module
+# After importing the module, you can access its functions, classes, and variables using the module name as a prefix. For example, if my_module.py contains a function called my_function(), you can call it like this:
+# my_module.my_function()
