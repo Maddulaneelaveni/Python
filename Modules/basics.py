840f9517ff110aad0c8c2c7f8489f98a9e2d2541
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-16 21:50:48 +0530

    file updated

--- Modules/basics.py
+++ Modules/basics.py
@@ -97,7 +97,7 @@ print(random.randint(1,10)) # Output: 7
 # The dir() function is a built-in function in Python that returns a list of the attributes
 # The dir() function shows everything inside a module.
 import math
-print(dir(math))  
+print(dir(math))  # output :
 
 
 
