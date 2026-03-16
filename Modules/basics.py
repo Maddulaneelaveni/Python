b4e225638ca762e25c8e2bb4153d564b390672b9
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-16 21:45:40 +0530

    file updated

--- Modules/basics.py
+++ Modules/basics.py
@@ -84,6 +84,7 @@ print(result)  # Output: 8
 # 2. random: Provides functions for generating random numbers and performing random operations.
 # 3. datetime: Provides classes for working with dates and times.
 # 4. os: Provides functions for interacting with the operating system, such as file and directory operations.
+# 5. sys: Provides functions for interacting with the Python interpreter and system-specific parameters.
 
 
 
