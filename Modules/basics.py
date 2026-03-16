6d119f090d76e6c60ada6e48a1a5aca8d68ea1a4
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-16 21:46:33 +0530

    file updated

--- Modules/basics.py
+++ Modules/basics.py
@@ -86,5 +86,8 @@ print(result)  # Output: 8
 # 4. os: Provides functions for interacting with the operating system, such as file and directory operations.
 # 5. sys: Provides functions for interacting with the Python interpreter and system-specific parameters.
 
+# Example :
+
+
 
 
