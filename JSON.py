be7dad67196c0aab6859e75857cce041210257a2
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-03-31 22:00:00 +0530

    file updated

--- JSON.py
+++ JSON.py
@@ -1,2 +1,7 @@
 # JSON
 # JSON (JavaScript Object Notation) is a lightweight data format used to store and exchange data.
+
+# It is:
+# Human-readable
+# Language-independent
+# Widely used in APIs, web apps, data pipelines
