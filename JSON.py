7b059c51c6639ed50722c62ff4e24badd76aef3b
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-05 22:00:00 +0530

    file updated

--- JSON.py
+++ JSON.py
@@ -97,6 +97,15 @@ import json
 data = {"name": "Neelaveni", "age": 22, "skills": ["Python", "SQL"]}
 pretty_json = json.dumps(data, indent=4)
 print(pretty_json)
+# Output :
+{
+    "name": "Neelaveni",
+    "age": 22,
+    "skills": [
+        "Python",
+        "SQL"
+    ]
+}
 
 
 
