bf3bda5b4c220eb9e1fb3d4616704229066fd07d
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-08 18:16:32 +0530

    file added

--- OOPs_concepts/Abstraction.py
+++ OOPs_concepts/Abstraction.py
@@ -2,3 +2,4 @@
 # Abstraction is an OOP concept where we hide internal implementation details and show only essential features to the user.
 # It allows us to focus on what an object does rather than how it does it.
 # In Python, we can achieve abstraction using abstract classes and methods from the `abc` module.
+from abc import ABC, abstractmethod
