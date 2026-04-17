f9f946a1328e676310aeb634243ca37c69b9cf83
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-17 22:00:00 +0530

    file updated

--- OOPs_concepts/Inheritance.py
+++ OOPs_concepts/Inheritance.py
@@ -21,6 +21,18 @@ obj.display()   # child class method
 # This is child class
 
 # Example problem:
+class Animal:
+    def eat(self):
+        print("Eating")
+    def sleep(self):
+        print("Sleeping")
+class Dog(Animal):   
+    def bark(self):
+        print("Barking")
+d = Dog()
+d.eat()  
+d.sleep()  
+d.bark() 
 
 # Types of Inheritance:
 
