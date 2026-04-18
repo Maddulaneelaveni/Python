e85360fa2394c2de711598a083cce76a31ca1464
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-18 22:00:00 +0530

    file updated

--- OOPs_concepts/Inheritance.py
+++ OOPs_concepts/Inheritance.py
@@ -15,6 +15,7 @@ class Child(Parent):
 obj = Child()
 obj.show()      # inherited method
 obj.display()   # child class method
+# In the above example, the Child class inherits from the Parent class, allowing it to use the show() method defined in the Parent class. The Child class can also have its own methods, such as display().
 
 
 
