9217ade7b11a9901b541761cf049bc3e2304691b
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-18 22:00:00 +0530

    file updated

--- OOPs_concepts/Inheritance.py
+++ OOPs_concepts/Inheritance.py
@@ -1,5 +1,9 @@
 # Inheritance is a fundamental concept in object-oriented programming (OOP) that allows a new class (called a child or subclass) to inherit properties and behaviors (attributes and methods) from an existing class (called a parent or superclass). 
 # This promotes code reusability and establishes a natural hierarchical relationship between classes.
+# It helps in:
+# Code reusability
+Reducing redundancy
+Creating a hierarchical relationship
 
 
 
