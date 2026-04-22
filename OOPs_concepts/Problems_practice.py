95d11f7cd7432549a9b63d62a768afd3b64c0433
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-22 22:00:00 +0530

    file added

--- OOPs_concepts/Problems_practice.py
+++ OOPs_concepts/Problems_practice.py
@@ -1,4 +1,4 @@
-# # 1. Class and Object
+#  1. Class and Object
 # Define a class named Student
 class Student:
     # Constructor to initialize object attributes
@@ -20,3 +20,21 @@ s3 = Student("C", 20, 88)
 s1.display()
 s2.display()
 s3.display()
+
+# 2. Constructor
+# # Define Employee class
+class Employee:
+    # Constructor to initialize values when object is created
+    def __init__(self, name, department, salary):
+        self.name = name              # Store employee name
+        self.department = department # Store department
+        self.salary = salary         # Store salary
+    # Method to display employee details
+    def show_details(self):
+        print(self.name, self.department, self.salary)
+# Create objects
+e1 = Employee("Neelaveni", "IT", 50000)
+e2 = Employee("Anu", "HR", 40000)
+# Display details
+e1.show_details()
+e2.show_details()
