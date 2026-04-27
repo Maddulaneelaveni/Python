f45be332aca319eca3d2820d435b534ea8fdde01
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-27 22:00:00 +0530

    file updated

--- OOPs_concepts/Problems_practice.py
+++ OOPs_concepts/Problems_practice.py
@@ -112,3 +112,10 @@ s.set_marks(-10)  # Invalid case
 
 
 # 7. Polymorphism (Method Overriding)
+# Create Parent class: Shape
+Method:
+draw()
+Create Child classes:
+Circle
+Rectangle
+Override draw() method.
