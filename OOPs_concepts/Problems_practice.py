1778ab9c4d5da16590a27c33b87b9e411f77f5b4
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-28 22:00:00 +0530

    file updated

--- OOPs_concepts/Problems_practice.py
+++ OOPs_concepts/Problems_practice.py
@@ -116,3 +116,17 @@ s.set_marks(-10)  # Invalid case
 # Method: draw()
 # Create Child classes: Circle ,Rectangle
 # Override draw() method.
+
+class Shape:
+    def draw(self):
+        print("Drawing Shape")
+class Circle(Shape):
+    def draw(self):
+        print("Drawing Circle")
+class Rectangle(Shape):
+    def draw(self):
+        print("Drawing Rectangle")
+c = Circle()
+r = Rectangle()
+c.draw()
+r.draw()
