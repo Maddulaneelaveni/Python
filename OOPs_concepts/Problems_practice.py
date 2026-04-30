0b7691ef0ed4a00c61736e7dacaf9c98dc2dc753
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-04-30 22:00:00 +0530

    file updated

--- OOPs_concepts/Problems_practice.py
+++ OOPs_concepts/Problems_practice.py
@@ -145,6 +145,5 @@ class Calculator:
     def add(self, a, b, c=0):
         return a + b + c
 calc = Calculator()
-
 print(calc.add(2, 3))       # 2 numbers
 print(calc.add(2, 3, 4))    # 3 numbers
